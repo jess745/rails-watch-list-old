aeaa803a639fcc926a93c921cb443b8eddb6c0e6
jessica coueron <jcoueron@yahoo.fr>
2023-01-10 20:09:05 +0100

    rails new

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,2 +1,40 @@
 class ListsController < ApplicationController
+  before_action find_list, only: %i[show destroy update]
+  def index
+    @lists = List.all
+  end
+
+
+  def show
+
+  end
+
+  def update
+    @list.update(list_params)
+  end
+
+  def destroy
+    @list.destroy
+    redirect_to lists_path, status: :see_other
+  end
+
+  def new
+    @list = List.new
+  end
+
+  def create
+    @list = List.new(movie_params)
+    @list.save
+    redirect_to list_path(@list)
+  end
+
+  private
+
+  def list_params
+    params.require(:list).permit(:name)
+  end
+
+  def find_list
+    @list = List.find(params[:id])
+  end
 end
--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,2 +1,32 @@
 class MoviesController < ApplicationController
+  def index
+    @movies = Movie.all
+  end
+
+  def show
+  end
+
+  def new
+    @movie = Movie.new
+  end
+
+  def create
+    @movie = Movie.new(movie_params)
+    @movie.save
+    redirect_to movie_path(@movie)
+  end
+
+  def destroy
+    @movie.destroy
+    redirect_to movies_path, status: see_other
+  end
+
+  def update
+    @movie.update(movie_params)
+
+  private
+
+  def movie_params
+    params.require(:movie).permit(:title, :overview, :poster_url, :rating)
+  end
 end
